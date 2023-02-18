41f56acec4a616ba29d991b587a18197f18588ff
Srihari0911 <122223722+Srihari0911@users.noreply.github.com>
2023-02-17 22:15:41 -0500

    Updated Die.kt class

--- Die.kt
+++ Die.kt
@@ -1,3 +1,9 @@
+/*
+This assignment was done in group with Jobin Koodathinkal Jose(A00250653)
+and myself Sri Hari Sreenivasan(A00272180)
+We both did brainstorming in developing the logic and debugging the code.
+*/
+
 import kotlin.random.Random
 
 class Die {
